206b12c138c6e186eed4064cac14b06602acf1e0
lucasrodrigues062 <lucasrodrigues062@gmail.com>
2022-10-11 20:43:33 -0300

    Concluido construcao de componentes

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,12 +1,20 @@
+import 'package:dw_fwc_album/app/core/ui/helpers/loader.dart';
+import 'package:dw_fwc_album/app/core/ui/helpers/messages.dart';
 import 'package:dw_fwc_album/app/core/ui/styles/button_styles.dart';
 import 'package:dw_fwc_album/app/core/ui/widgets/button.dart';
 import 'package:dw_fwc_album/app/core/ui/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:dw_fwc_album/app/core/ui/styles/text_styles.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,18 +23,26 @@ class SplashPage extends StatelessWidget {
         child: Column(
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                showLoader();
+                await Future.delayed(const Duration(seconds: 2));
+                hideLoader();
+              },
               style: ButtonStyles.instance.yellowbutton,
               child: const Text('salvar'),
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                showError('erro no butao');
+              },
               style: ButtonStyles.instance.yellowOutlinebutton,
               child: const Text('salvar'),
             ),
             const TextField(),
             Button(
-              onPressed: () {},
+              onPressed: () {
+                showSuccess('info');
+              },
               style: ButtonStyles.instance.primarybutton,
               label: 'salvar',
               labelStyle: context.texStyles.textPrimaryFontBold,
